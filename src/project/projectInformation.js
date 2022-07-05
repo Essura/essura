ddc9aeac1ef2282896966e3eb549d8099855c514
Marcus Lau <mlau5@csc.com>
2022-07-06 08:54:03 +1000

    UPDATE project settings call

--- src/project/projectInformation.js
+++ src/project/projectInformation.js
@@ -1,33 +1,35 @@
 function setProjectInformation() {
-    const phaseNames = projectSetting["Items"].find(obj => {return obj.Setting.S === 'Project#Phase'})["PhaseNames"]["M"]
-    console.log(projectDetail)
-    const phaseNameMap = {
-        1: phaseNames["One"]["S"],
-        2: phaseNames["Two"]["S"],
-        3: phaseNames["Three"]["S"],
-        4: phaseNames["Four"]["S"],
-        5: phaseNames["Five"]["S"],
-    }
+    getProjectSettings().then(projectSetting => {
+        const phaseNames = projectSetting["Items"].find(obj => {return obj.Setting.S === 'Project#Phase'})["PhaseNames"]["M"]
+        console.log(projectDetail)
+        const phaseNameMap = {
+            1: phaseNames["One"]["S"],
+            2: phaseNames["Two"]["S"],
+            3: phaseNames["Three"]["S"],
+            4: phaseNames["Four"]["S"],
+            5: phaseNames["Five"]["S"],
+        }
 
-    const prevPhase = currentPhase - 1
-    let currentFundingApproved = "0"
-    let targetCompletionDate = "N/A"
-    if (prevPhase > 0) {
-        currentFundingApproved = getPhaseFieldValue(currentPhase, "Investment").Complete.S
-        targetCompletionDate = getPhaseFieldValue(currentPhase, "Time").EndDate.S
-    }
+        const prevPhase = currentPhase - 1
+        let currentFundingApproved = "0"
+        let targetCompletionDate = "N/A"
+        if (prevPhase > 0) {
+            currentFundingApproved = getPhaseFieldValue(currentPhase, "Investment").Complete.S
+            targetCompletionDate = getPhaseFieldValue(currentPhase, "Time").EndDate.S
+        }
 
-    console.log(currentFundingApproved)
-    console.log(phaseNameMap[currentPhase])
-    console.log(targetCompletionDate)
-    console.log(prevPhase)
-    console.log(currentPhase)
+        console.log(currentFundingApproved)
+        console.log(phaseNameMap[currentPhase])
+        console.log(targetCompletionDate)
+        console.log(prevPhase)
+        console.log(currentPhase)
 
 
-    $(".currentphase").text(phaseNameMap[currentPhase])
-    $(".owner").text(projectDetail["Owner"]["S"])
-    $(".lastupdate").text(formatDate(projectDetail["LastUpdate"]["N"]))
-    $(".currentestimatedtotal").text(currentFundingApproved)
-    $(".currentfundingapproved").text(currentFundingApproved)
-    $(".targetcompletiondate").text(targetCompletionDate)
+        $(".currentphase").text(phaseNameMap[currentPhase])
+        $(".owner").text(projectDetail["Owner"]["S"])
+        $(".lastupdate").text(formatDate(projectDetail["LastUpdate"]["N"]))
+        $(".currentestimatedtotal").text(currentFundingApproved)
+        $(".currentfundingapproved").text(currentFundingApproved)
+        $(".targetcompletiondate").text(targetCompletionDate)
+    })
 }
